1dbad112197e7dd5777ee4b4346ab484c9573035
Rafael Soares <rafael19euro@gmail.com>
2024-11-13 18:29:58 -0300

    Updated fidelidade.

---
+++ js/dados.js
@@ -0,0 +1,101 @@
+var MENU = {
+	"barba": [
+        {
+            "id": "1",
+            "name": "Zero Beard",
+            "img":"./img/barba.jpg",
+            "price": 15,
+            "points": "35"
+        },
+        {
+            "id": "2",
+            "name": "Faded Beard",
+            "img":"./img/barba2.jpg",
+            "price": 30,
+            "points": "50"
+        },
+        {
+            "id": "3",
+            "name": "Handlebar",
+            "img":"./img/handle.jpg",
+            "price": 25,
+            "points": "45"
+        },
+
+    ],
+
+    "cortes": [
+    	{
+            "id": "1",
+            "name": "Social",
+            "img":"./img/corte1.jpg",
+            "price": 30,
+            "points": "50"
+        },
+        {
+            "id": "2",
+            "name": "Degrades",
+            "img":"./img/corte2.jpg",
+            "price": 40,
+            "points": "60"
+        },
+        {
+            "id": "3",
+            "name": "Corte + Desenho",
+            "img":"./img/corte3.jpg",
+            "price": 45,
+            "points": "65"
+        },
+
+    ],
+
+    "quimica": [
+    	{
+            "id": "1",
+            "name": "Nevou",
+            "img":"./img/quimica1.jpeg",
+            "price": 55,
+            "points": "75"
+        },
+        {
+            "id": "2",
+            "name": "Progressiva",
+            "img":"./img/quimica2.png",
+            "price": 70,
+            "points": "90"
+        },
+        {
+            "id": "3",
+            "name": "Tintura",
+            "img":"./img/quimica3.jpeg",
+            "price": 45,
+            "points": "65"
+        },
+
+    ],
+
+    "combos": [
+    	{
+            "id": "1",
+            "name": "Barba + Corte + Somb.",
+            "img":"./img/combo1.jpeg",
+            "price": 55,
+            "points": "75"
+        },
+        {
+            "id": "2",
+            "name": "Corte + Progr./Quimica",
+            "img":"./img/cobo2.jpg",
+            "price": 90,
+            "points": "110"
+        },
+        {
+            "id": "3",
+            "name": "Geral",
+            "img":"./img/combo3.jpeg",
+            "price": 130,
+            "points": "150"
+        },
+
+    ]
+}
--- js/script.js
+++ js/script.js
@@ -1,28 +1,87 @@
-$(document).ready(function() {
-
-
+$(document).ready(function () {
+	servicos.eventos.init();
 })
 
-$('.barba-menu').click(() => {
-		$('#Serviços-barba').removeClass('.hidden')
-		$('#Serviços-corte').addClass('.hidden')
-		$('.quimica').addClass('.hidden')
-		$('.combo').addClass('.hidden')
-	})
+var servicos = {};
 
-	$('.corte-menu').click(() => {
-		$('#Serviços-corte').removeClass('.hidden')
-		$('#Serviços-barba').addClass('.hidden')
-		$('.quimica').addClass('.hidden')
-		$('.combo').addClass('.hidden')
-	})
+servicos.eventos = {
 
-	$('.quimica-menu').click(() => {
-		$('.quimica').removeClass('.hidden')
-		$('#Serviços-barba').addClass('.hidden')
-		$('#Serviços-corte').addClass('.hidden')
-		$('.combo').addClass('.hidden')
-	})
+	init: () => {
+		servicos.metodos.obterItensServicos();
+	}
+
+}
+
+servicos.metodos = {
+
+	obterItensServicos: (categoria = 'barba', vermais = false) => {
+
+		var filtro = MENU[categoria];
+
+		if (!vermais) {
+			$("#itensServicos").html('');
+			$("#btnVerMais").removeClass('hidden');
+		}
+
+		$.each(filtro, (i, e) => {
+
+			let temp = servicos.templates.item.replace(/\${img}/g, e.img)
+			.replace(/\${nome}/g, e.name)
+			.replace(/\${preco}/g, e.price.toFixed(2).replace('.', ','))
+			.replace(/\${pontos}/g, e.points)
+			.replace(/\${id}/g, e.id)
+
+
+			if (vermais && i >= 8 && i < 100) {
+				$("#itensServicos").append(temp)
+			}
+
+			if (!vermais && i < 8) {
+				$("#itensServicos").append(temp)
+			}
+
+		})
+
+		//remove active
+		$(".container-menu a").removeClass('active');
+
+		//seta o active
+		$("#menu-" + categoria).addClass('active')
+
+	},
+
+	verMais: () => {
+
+		var ativo = $(".container-menu a.active").attr('id').split('menu-')[1];
+		grupos.metodos.obterItensServicos(ativo, true);
+
+		$("#btnVerMais").addClass('hidden');
+	},
+
+}
+
+servicos.templates = {
+	item: `
+	<div class="col-sm-3 col-9 mb-5"  id="itensServicos">
+		<div class="card card-item" id="\${id}">
+                  <div class="img-produto">
+                    <img src="\${img}"/ class="imagem-prod">
+                    <p class="title-produto text-center mt-4">
+                    <b>\${nome}</b>
+                  </p>
+                  <p class="price-produto text-center">
+                    <b>R$ \${preco}</b>
+                  </p>
+                  <div class="add-agendar">
+                    <span class="btn btn-add mb-2"><a href="https://wa.me/5511990155204?text=Olá,+quero+agendar+um+horário+para+\${nome},+lembre+que+estou+ganhando+\${pontos}+pontos!" target="_blank">Agendar</a></span><br>
+                  </div>
+                    <p class="texto-pontos mt-1">Estou recbendo \${pontos} pontos ao agendar!</p>
+                  </div>
+                  
+        </div>
+    </div>         
+	`
+}
 
 	$('.combo-menu').click(() => {
 		$('.combo').removeClass('.hidden')
